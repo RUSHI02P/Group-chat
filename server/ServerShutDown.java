24acd79faf469c08db13c07f9802171655786f96
RUSHI02P <49794032+RUSHI02P@users.noreply.github.com>
2023-02-25 02:56:39 -0500

    Update ServerShutDown.java

--- server/ServerShutDown.java
+++ server/ServerShutDown.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package server;
 
 import java.io.DataInputStream;
@@ -12,10 +8,6 @@ import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
- * @author wmall
- */
 public class ServerShutDown extends Thread {
 
     @Override
