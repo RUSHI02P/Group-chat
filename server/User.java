d76e6aae4aa7c86b623c4e956a7271e8237aecd0
RUSHI02P <49794032+RUSHI02P@users.noreply.github.com>
2023-02-25 02:55:47 -0500

    Update User.java

--- server/User.java
+++ server/User.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package server;
 
 import java.io.BufferedInputStream;
@@ -13,10 +9,7 @@ import java.net.Socket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
     
-/**
- *
- * @author Mohsin Hayat
- */
+
 public class User extends Thread{ //User Thread class.
 
     String name;
